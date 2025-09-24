8ff369c8fc8b40694809e90123fbc3ebe7386548
alexcruz097 <alexc017@yahoo.com>
2025-09-24 11:50:29 -0700

    bot is working and making response based on what the user inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import nodeCrypto from "crypto";
 
 import "./App.css";
 import Nav from "./components/Nav";
@@ -6,37 +7,81 @@ import Form from "./components/Form";
 import ChatBox from "./components/ChatBox";
 function App() {
   const [inputPosition, setInputPosition] = useState(true);
-  const [userInput, setUserInput] = useState("");
-  const userChat = [];
+  const [chatMessages, setChatMessages] = useState([]);
   const handlePositionChange = () => {
     setInputPosition(!inputPosition);
   };
-  const getUserInput = (input) => {
-    setUserInput(input);
-    console.log(input);
-  };
 
-  const createUserChat = () => {
-    console.log("clicked");
-    return <ChatBox input={userInput} />;
-  };
+  const createUserChat = (userInput) => {
+    setChatMessages((prevMessages) => {
+      return [
+        ...prevMessages,
+        { sender: "user", message: userInput, id: crypto.randomUUID() },
+      ];
+    });
+    // check user input and set bot response
 
+    switch (true) {
+      case userInput.toLowerCase().trim().includes("hello"):
+        setTimeout(() => {
+          setChatMessages((prevMessages) => {
+            return [
+              ...prevMessages,
+              {
+                sender: "bot",
+                message: "Hi there! How can I help you",
+                id: crypto.randomUUID(),
+              },
+            ];
+          });
+        }, 1000);
+        break;
+      case userInput.toLowerCase().includes("date"):
+        setTimeout(() => {
+          setChatMessages((prevMessages) => {
+            return [
+              ...prevMessages,
+              {
+                sender: "bot",
+                message: "today is " + new Date().toLocaleDateString(),
+                id: crypto.randomUUID(),
+              },
+            ];
+          });
+        }, 1000);
+        break;
+
+      default:
+      case userInput.includes("date"):
+        setTimeout(() => {
+          setChatMessages((prevMessages) => {
+            return [
+              ...prevMessages,
+              {
+                sender: "bot",
+                message: "Sorry, I didn't understand that. Try again!",
+                id: crypto.randomUUID(),
+              },
+            ];
+          });
+        }, 1000);
+        break;
+    }
+  };
   return (
     <>
       <Nav />
       <div className="flex flex-col justify-center content-center flex-wrap">
-        {inputPosition === true ? (
-          <Form getUserInput={getUserInput} createUserChat={createUserChat} />
-        ) : (
-          ""
-        )}
-        <ChatBox input={userInput} sender="user"/>
-        <ChatBox input={userInput} sender="robot"/>
-        <ChatBox input={userInput} sender="user"/>
-        <ChatBox input={userInput} sender="robot"/>
+        {inputPosition === true ? <Form createUserChat={createUserChat} /> : ""}
+        {/* iterate thru the array of objects */}
+        {chatMessages.map((chat) => (
+          <>
+            <ChatBox key={chat.id} input={chat.message} sender={chat.sender} />
+          </>
+        ))}
 
         {inputPosition === false ? (
-          <Form getUserInput={getUserInput} createUserChat={createUserChat} />
+          <Form createUserChat={createUserChat} />
         ) : (
           ""
         )}
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 import React, { use } from "react";
 import { useState } from "react";
-function Form({ getUserInput, createUserChat }) {
+function Form({ createUserChat }) {
   const [userInput, setUserInput] = useState("");
   return (
     <div className="flex justify-center centent-center mt-6">
@@ -8,6 +8,7 @@ function Form({ getUserInput, createUserChat }) {
         type="text"
         name=""
         id=""
+        value={userInput}
         className="border-2 border-black p-2 mr-2 rounded-lg w-80"
         placeholder="Send a message to Chatbot"
         onChange={(e) => {
@@ -17,8 +18,11 @@ function Form({ getUserInput, createUserChat }) {
       <button
         type="submit"
         onClick={(e) => {
-          getUserInput(userInput);
-          createUserChat(userInput);
+          // check if userInput is not empty or just spaces
+          if (userInput.trim() !== "") {
+            createUserChat(userInput);
+            setUserInput("");
+          }
         }}
         className="bg-green-500 text-white p-2 border-none w-32
  rounded-lg
